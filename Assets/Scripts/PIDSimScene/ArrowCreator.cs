545a1e672ec6198a6cc87a0179697267e9ddb091
Kaden C <63522451+Kadition@users.noreply.github.com>
2025-02-24 22:31:28 -0600

    no peaking 2

---
+++ Assets/Scripts/PIDSimScene/ArrowCreator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ArrowCreator : MonoBehaviour
+{
+    // TODO - for every scale of 1, add 0.5 to x positon on arrow
+    // TODO - every fixed frame set the position of the arrow to the position of the arrowposition thingy
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
