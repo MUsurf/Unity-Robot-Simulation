721f11fc4d18e556235b6bd0776d31f6b3d4a8ee
Kaden C <63522451+Kadition@users.noreply.github.com>
2025-03-06 15:02:46 -0600

    front avoidance system works

--- Assets/Scripts/RobotSimScene/AI Robot/AIBehaviour.cs
+++ Assets/Scripts/RobotSimScene/AI Robot/AIBehaviour.cs
@@ -5,10 +5,13 @@ public class AIBehaviour : MonoBehaviour
     public GameObject PlayerBot;
     public GameObject AIBot;
     public int distance = 10;
+    public int rayDistance = 5;
     public bool aiEnabled = true;
     public bool followPlayer = true;
     public AIPID pidScript;
     public GameObject movePoint;
+    private Vector3 frontVector = new Vector3(-3.4f, 1.5f, 6.2f);
+    
 
     // Update is called once per frame
     void FixedUpdate()
@@ -28,9 +31,57 @@ public class AIBehaviour : MonoBehaviour
 
             Debug.Log($"wantedPosition: {wantedPosition:F10}, directionVector: {directionVector.normalized:F10}");
 
-            Physics.Raycast(Vector3 origin, Vector3 direction, out RaycastHit hitInfo, float maxDistance, int layerMask, QueryTriggerInteraction queryTriggerInteraction);
+            Vector3 modifiedVector = frontVector;
 
+            float closestHit = rayDistance + 1;
 
+            bool hit;
+
+            int flip = 1;
+
+            // flip = 0;
+            // while(flip != -1)
+            // {
+            //     if(flip == 0)
+            //     {
+            //         flip = 1;
+            //     }
+            //     else
+            //     {
+            //         flip = -1;
+            //     }
+
+            //     frontVector.y *= flip;
+            // }
+
+            for(int i = 0; i < 3; i++)
+            {
+                modifiedVector.y = frontVector.y - i * 1.5f;
+                for(int j = 0; j < 3; j++)
+                {
+                    modifiedVector.x = frontVector.x + j * 3.4f;
+                    hit = Physics.Raycast(modifiedVector + AIBot.transform.position, Vector3.forward * flip, out RaycastHit hitInfo, rayDistance, 0);
+
+                    Debug.DrawRay(modifiedVector + AIBot.transform.position, Vector3.forward * rayDistance * flip, Color.red, 0);
+
+                    if(hit && hitInfo.distance < closestHit)
+                    {
+                        closestHit = hitInfo.distance;
+                    }
+                }
+            }
+
+            if(closestHit != rayDistance + 1)
+            {
+                wantedPosition.z = -(rayDistance - closestHit) + AIBot.transform.position.z;
+            }
+        
+
+            //-1.5 to 1.5 y
+
+            //-6.2 to 6.2 z
+
+            //-3.4 to 3.4 x
 
 
 
